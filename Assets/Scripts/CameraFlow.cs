bb347a180c2103d244d3e3e811b202d8fce6c41a
pzdcTIP26 <timurpahomo26@gmail.com>
2022-10-23 17:45:56 +0300

    release v2

--- Assets/Scripts/CameraFlow.cs
+++ Assets/Scripts/CameraFlow.cs
@@ -5,6 +5,7 @@ public class CameraFlow : MonoBehaviour
 {
     [SerializeField] private Transform _cameraCenter;
     [SerializeField] private float _speedOfflow;
+    private  float _speed = 1f;
 
     private void Update()
     {
